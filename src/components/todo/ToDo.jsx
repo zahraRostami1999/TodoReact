7bac344719e4d470c693b48c7b2e1d37c15db4b2
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-06 20:39:26 +0330

    edit Todo component: add handleInput change for get input and addTask for adding new input task to tasks list

--- src/components/todo/ToDo.jsx
+++ src/components/todo/ToDo.jsx
@@ -4,14 +4,23 @@ import styles from "./ToDo.module.css";
 function ToDo() {
   const [tasks, setTasks] = useState(["Try React Project", "Eat Bread"]);
   const [newTask, setNewTask] = useState("");
+  
+  const handleInputChange = (e) =>{
+    setNewTask(e.target.value);
+  }
+
+  const addTask = () => {
+    setTasks(t =>[...t, newTask]);
+    setNewTask("")
+  }
 
   return (
     <>
       <div className={styles.container}>
         <h1>To-Do List</h1>
         <div className={styles.inputTask}>
-          <input type="text" placeholder="Add a new task" />
-          <button>Add Task</button>
+          <input name="inputTask" type="text" placeholder="Add a new task" value={newTask} onChange={(e) => handleInputChange(e)}/>
+          <button onClick={addTask}>Add Task</button>
         </div>
         <div className={styles.tasksList}>
           <ol>
