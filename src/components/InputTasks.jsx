e4ef4c71e05a66bc42e3d76b1d758a3edb23fbc4
Zahra Rostami <zahrarostami137878@gmail.com>
2024-11-29 10:44:19 +0330

    edit InputTasks: add Input and AddButton components

---
+++ src/components/InputTasks.jsx
@@ -0,0 +1,15 @@
+import Input from "./input";
+import AddButton from "./AddButton";
+
+const InputTasks = () => {
+    return (
+        <>
+            <div>
+                <Input />
+                <AddButton />
+            </div>
+        </>
+    );
+}
+
+export default InputTasks;
