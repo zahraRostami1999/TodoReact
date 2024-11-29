4fc74bcbb58fb633c496bad9191dc264796e1ff7
Zahra Rostami <zahrarostami137878@gmail.com>
2024-11-29 10:45:07 +0330

    edit Header: add h1 and h2 for Todo app tittle

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <>
+            <div>
+                <h1 className="sm:text-center sm:text-2xl md:text-left font-bold">Xanim Qiz To-Do List</h1>
+                <h2 className="sm:text-center sm:text-xl md:text-left font-semibold">Just Do It Babe 🥰</h2>
+            </div>
+        </>
+    );
+}
+
+export default Header;
