06d599ee4d624e89fb0ee65e425aea3540f97e07
Zahra Rostami <zahrarostami137878@gmail.com>
2024-11-29 16:48:31 +0330

    edit Input: add tasks that inputed to newTask in redux

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,21 @@
+import { setNewTask } from "../redux/TaskSlice";
+import { useDispatch, useSelector } from "react-redux";
+
 const Input = () => {
-    return ( 
+    const taskTitle = useSelector((state) => state.Task.newTask)
+    const dispatch = useDispatch();
+
+    const handleInputChange = (e) => {
+        const task = e.target.value;
+        dispatch(setNewTask(task))
+    }
+    return (
         <>
-        <div>
-            <input type="text" className="border"/>
-        </div>
+            <div>
+                <input type="text" className="border" value={taskTitle} onChange={(e) => handleInputChange(e)} />
+            </div>
         </>
-     );
+    );
 }
- 
+
 export default Input;
