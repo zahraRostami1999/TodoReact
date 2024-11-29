c764f4471cd10913e1935d8a7970d1b0e49d13ad
Zahra Rostami <zahrarostami137878@gmail.com>
2024-11-29 10:46:30 +0330

    edit App: add TodoPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import ToDo from './components/todo/ToDo.jsx';
+import TodoPage from './pages/TodoPage';
 
 function App() {
   return (
     <div className="App">
-      <ToDo />
+      <TodoPage />
     </div>
   );
 }
