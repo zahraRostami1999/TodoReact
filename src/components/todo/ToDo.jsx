b8e108d5bf032eed3f30b5e78ce76a1b03048bc7
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-03 23:07:29 +0330

    edit ToDo

--- src/components/todo/ToDo.jsx
+++ src/components/todo/ToDo.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import styles from "./todoStyle.module.css"
 import checkedImg from "../../assets/checked.png";
 import binImg from '../../assets/bin.png';
 import upImg from "../../assets/up.png";
@@ -69,48 +70,52 @@ function ToDo() {
 
   return (
     <>
-      <div className={`w-full flex justify-center pb-60  text-neutral-800`}>
+    {/* <div className={styles.bg}>
+Hello
+    </div> */}
+      <div className={`w-full flex justify-center pb-60 text-neutral-800 ${tasks.length>6 ? "h-[1500px]": "h-[900px]"}  bg-gradient-to-br from-purple-200 to-purple-400`}>
         <div className={`mt-5 w-full flex justify-center flex-wrap`}>
           <div className="py-1 font-sans font-semibold flex justify-center flex-w sm:w-full md:w-11/12 lg:w-5/6">
-            <div className="w-full sm:text-center xs:text-center md:text-left lg:text-left">
-              <h1 className="sm:text-2xl md:text-2xl lg:text-3xl font-bold">Xanim Qiz To-Do List</h1>
-              <h2 className="sm:text-xl md:text-xl lg:text-2xl font-semibold">Just Do It Babe 🥰</h2>
+            <div className="w-full sm:text-center xs:text-center md:text-left lg:text-left ">
+              <h1 className="sm:text-3xl md:text-4xl lg:text-5xl font-bold ">Xanim Qiz To-Do List</h1>
+              <h2 className="sm:text-2xl md:text-3xl lg:text-4xl font-semibold">Just Do It Babe 🥰</h2>
             </div>
 
           </div>
-          <div className="py-1 w-full flex flex-wrap items-start absolute top-24 pb-20">
-            <div className="w-full flex justify-center h-10 text-neutral-700  ">
-              <div className=" xs:w-11/12 sm:w-11/12 md:w-9/12 lg:w-9/12 xl:w-10/12 flex rounded-full overflow-hidden shadow-zinc-600 shadow-md focus-within:outline focus-within:outline-blackBg">
+          <div className="py-1 w-full flex flex-wrap items-start absolute top-36 pb-20 ">
+            <div className="w-full flex justify-center h-16 text-neutral-700 ">
+              <div className=" xs:w-11/12 sm:w-11/12 md:w-9/12 lg:w-9/12 xl:w-8/12 bg-white flex rounded-full overflow-hidden shadow-zinc-600 shadow-md focus-within:outline focus-within:outline-blackBg">
                 <div className="w-11/12 ">
-                  <input type="text" className="w-full h-full outline-none bg-orange-100   px-4 text-lg" placeholder="Add your new task..."
+                  <input type="text" className="w-full h-full outline-none  px-7 text-2xl font-semibold" placeholder="Add your new task..."
                     value={newTask.title}
                     onChange={handleInputChange} />
                 </div>
-                <div className="md:w-1/12 sm:w-2/12 xs:w-2/12 flex justify-center font-bold items-center bg-orange-500 active:bg-orange-600" onClick={addTask}>
-                  <button>+</button>
+                <div className="md:w-2/12 sm:w-2/12 xs:w-2/12 flex justify-center rounded-lg font-bold text-2xl items-center bg-orange-400 active:bg-orange-600 hover:bg-orange-500 translation-all duration-200" onClick={addTask}>
+                  <button className="hover:text-3xl">+</button>
                 </div>
               </div>
             </div>
+            
             <div className=" w-full flex justify-center items-start mt-20  h-full ">
-              <ol className={`w-11/12 min-h-10 mb-20  px-5 py-6 rounded-md bg-blackBg shadow-xl shadow-gray-400 `}>
-                {tasks.length === 0 ? <p className="text-center font-bold">✨Your Tasks Place✨</p> : <p></p>}
+              <ol className={`w-10/12 mb-20  px-5 py-6 rounded-md bg-blackBg shadow-xl shadow-gray-400 `}>
+                {tasks.length === 0 ? <p className="text-center font-bold lg:text-2xl">✨Your Tasks Place✨</p> : <p></p>}
                 {tasks.map((task, index) => (
                   <li
                     key={index}
-                    className="flex justify-between my-2.5 rounded-full bg-orange-100 shadow-zinc-400 shadow-md sm:px-2 py-1 relative hover:scale-x-105 transition-all duration-300">
+                    className="flex w-11/12 mx-auto justify-between my-2.5 rounded-full bg-orange-100 shadow-zinc-400 shadow-md sm:px-2 py-3 relative hover:bg-orange-200 transition-all duration-300">
                     <div className="w-5/6 flex mt">
                       <span>💫</span>
                       <p
                         style={{
                           textDecoration: task.done ? "line-through" : "none",
                         }}
-                        className="text-base font-medium sm:w-1/2 w-11/12 break-words whitespace-normal"
+                        className="text-lg font-medium sm:w-1/2 w-11/12 break-words whitespace-normal"
                       >
                         {task.title}
                       </p>
                     </div>
                     <div
-                      className={` flex justify-center items-center rounded-full  ${activeButton === index
+                      className={` flex justify-center items-center rounded-full text-xl  ${activeButton === index
                         ? "bg-orange-400 hover:text-zinc-100 text-zinc-100"
                         : "text-purple-800 hover:text-purple-950"
                         } px-2.5 cursor-pointer relative`}
@@ -119,10 +124,10 @@ function ToDo() {
                       ⫶
                     </div>
                     {visibleDropDownIndex === index && (
-                      <div className=" w-8 absolute sm:-right-8 -top-6 mt-6 rounded-full shadow-2xl shadow-gray-100  bg-nu-200" >
+                      <div className=" lg:w-12 sm:w-9 md:w-9 absolute sm:-right-10 lg:-right-12 -top-6 mt-6 rounded-full shadow-2xl shadow-gray-100  bg-neutral-200" >
                         <ul className="">
                           <li
-                            className="px-2 py-1 hover:bg-orange-300 cursor-pointer rounded-md"
+                            className="px-2 py-1 hover:bg-orange-300 cursor-pointer rounded-full"
                             onClick={() => {
                               doneTask(index);
                               toggleDropDown(null);
@@ -131,7 +136,7 @@ function ToDo() {
                             <img src={checkedImg} alt="" />
                           </li>
                           <li
-                            className="px-2 py-1 hover:bg-orange-300 cursor-pointer rounded-md"
+                            className="px-2 py-1 hover:bg-orange-300 cursor-pointer rounded-full"
                             onClick={() => {
                               deleteTask(index);
                               toggleDropDown(null);
@@ -140,7 +145,7 @@ function ToDo() {
                             <img src={binImg} alt="" />
                           </li>
                           <li
-                            className="px-2 py-1 hover:bg-orange-300 cursor-pointer rounded-md"
+                            className="px-2 py-1 hover:bg-orange-300 cursor-pointer rounded-full"
                             onClick={() => {
                               topTask(index);
                               toggleDropDown(null);
@@ -149,7 +154,7 @@ function ToDo() {
                             <img src={upImg} alt="" />
                           </li>
                           <li
-                            className="px-2 py-1 hover:bg-orange-300 cursor-pointer rounded-md"
+                            className="px-2 py-1 hover:bg-orange-300 cursor-pointer rounded-full"
                             onClick={() => {
                               bottomTask(index);
                               toggleDropDown(null);
