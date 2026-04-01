f2c45e302d05a685ccf6370950657380163a1214
Zahra Rostami <zahrarostami137878@gmail.com>
2026-04-01 09:58:36 +0330

    sadra's change

--- src/services/API.jsx
+++ src/services/API.jsx
@@ -1,83 +1,143 @@
+import ErrMsg from "../config/errorMessages"
 
-const base_url = "http://todo.test/api/v1";
+export default class Auth {
+    static #base_url = "http://todo.souperlopers.ir/api/v1"
+    refreshInterval = undefined
 
-export const checkUserName = async (username) => {
-    try {
-        const response = await fetch(`${base_url}/users/${username}`, {
-            method: "GET",
-        });
-        const result = await response.json();
-        return result;
-    } catch (error) {
-        console.log("Error checking username:", error);
-        throw error;
+    static async checkUserName(username) {
+        // define endpoint and options
+        const ep = `users/${username}`
+        const method = "GET"
+
+        const result = await this.#sendRequest(ep, method)
+
+        // error
+        if (!result.ok && result.status !== 404) {
+            return result
+        }
+
+        const body = await result.json()
+        const userExists = body.user_exists
+
+        return userExists
+    }
+    
+    static login() {}
+    static logout() {}
+    static createUser() {}
+    refresh() {
+        this.refreshInterval = setInterval(this.#refreshOperation)
+    }
+    #refreshOperation() {}
+    stopRefresh = () => clearInterval(this.refreshInterval)
+
+    static async #sendRequest(ep, method, headers = {}, body = {}) {
+        // endpoint url
+        const endpoint = `${this.#base_url}/${ep}`
+
+        // fetch options
+        const options = { method: method.trim().toUpperCase(), headers }
+
+        // add contenttype header if there is body
+        if (Object.keys(body).length) {
+            options.body = body
+            options.headers["Content-Type"] = "application/json"
+        }
+
+        try {
+            // send request
+            return await fetch(endpoint, options)
+
+            // connection error
+        } catch (error) {
+            console.error(error)
+            return { ok: false, message: ErrMsg.COMMON.CNN }
+        }
     }
 }
 
-//image/png
+// const base_url = "http://todo.test/api/v1"
 
-export const login = async (haveAccount, username, password) => {
-    try {
-        const endpoint = haveAccount ? `${base_url}/auth/login` : `${base_url}/user/`
-        const response = await fetch(endpoint, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-                "username": username,
-                "password": password
-            }),
-        });
-        const result = await response.json();
-        return result;
-    } catch (error) {
-        console.log("Error login:", error);
-        throw error;
-    }
-}
+// export const checkUserName = async (username) => {
+// 	try {
+// 		const response = await fetch(`${base_url}/users/${username}`, {
+// 			method: "GET",
+// 		})
+// 		const result = await response.json()
+// 		return result
+// 	} catch (error) {
+// 		console.log("Error checking username:", error)
+// 		throw error
+// 	}
+// }
 
-let refreshInterval = null;
+// //image/png
 
-export const refreshToken = async () => {
-    try {
-        const refresh = localStorage.getItem("refresh");
-        if (!refresh) return null;
+// export const login = async (haveAccount, username, password) => {
+// 	try {
+// 		const endpoint =
+// 			haveAccount ? `${base_url}/auth/login` : `${base_url}/user/`
+// 		const response = await fetch(endpoint, {
+// 			method: "POST",
+// 			headers: { "Content-Type": "application/json" },
+// 			body: JSON.stringify({
+// 				username: username,
+// 				password: password,
+// 			}),
+// 		})
+// 		const result = await response.json()
+// 		return result
+// 	} catch (error) {
+// 		console.log("Error login:", error)
+// 		throw error
+// 	}
+// }
 
-        const response = await fetch(`${base_url}/auth/refresh`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ refresh }),
-        });
-        // (200 <= respopnse status < 299) ===> response.ok=true
-        if (!response.ok) {
-            stopAutoRefresh();
-            localStorage.removeItem("access");
-            localStorage.removeItem("refresh");
-            window.location.href = "/login";
-            return null;
-        }
+// let refreshInterval = null
 
-        const result = await response.json();
+// export const refreshToken = async () => {
+// 	try {
+// 		const refresh = localStorage.getItem("refresh")
+// 		if (!refresh) return null
 
-        if (result?.access) {
-            localStorage.setItem("access", result.access);
-        }
+// 		const response = await fetch(`${base_url}/auth/refresh`, {
+// 			method: "POST",
+// 			headers: { "Content-Type": "application/json" },
+// 			body: JSON.stringify({ refresh }),
+// 		})
+// 		// (200 <= respopnse status < 299) ===> response.ok=true
+// 		if (!response.ok) {
+// 			stopAutoRefresh()
+// 			localStorage.removeItem("access")
+// 			localStorage.removeItem("refresh")
+// 			window.location.href = "/login"
+// 			return null
+// 		}
 
-        return result;
-    } catch (error) {
-        console.log("Refresh error:", error);
-    }
-};
+// 		const result = await response.json()
 
-export const startAutoRefresh = () => {
-    stopAutoRefresh();
+// 		if (result?.access) {
+// 			localStorage.setItem("access", result.access)
+// 		}
 
-    refreshInterval = setInterval(() => {
-        refreshToken();
-    }, 13 * 60 * 1000);
-};
+// 		return result
+// 	} catch (error) {
+// 		console.log("Refresh error:", error)
+// 	}
+// }
 
+// export const startAutoRefresh = () => {
+// 	stopAutoRefresh()
 
-export const stopAutoRefresh = () => {
-    if (refreshInterval) clearInterval(refreshInterval);
-    refreshInterval = null;
-};
+// 	refreshInterval = setInterval(
+// 		() => {
+// 			refreshToken()
+// 		},
+// 		13 * 60 * 1000,
+// 	)
+// }
+
+// export const stopAutoRefresh = () => {
+// 	if (refreshInterval) clearInterval(refreshInterval)
+// 	refreshInterval = null
+// }
