8c5c032c6889974c696a44f11ded2e70b8160053
Zahra Rostami <zahrarostami137878@gmail.com>
2026-04-01 10:40:22 +0330

    v2

--- src/components/login/GetUsername.jsx
+++ src/components/login/GetUsername.jsx
@@ -1,76 +1,66 @@
-import React, { useState } from 'react';
-import FloatInput from '../input/FloatInput';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react"
+import FloatInput from "../FloatInput"
+import { Link } from "react-router-dom"
+import ErrMsg from "../../config/errorMessages"
+import Const from "../../config/constants"
+import LoginButton from "../LoginButton"
 
-function GetUsername({ data, handleChangeInput, handleClickButton }) {
-    const [error, setError] = useState();
-    const min_length = 4;
-    const max_length = 18;
+export default function GetUsername({
+    userInput,
+    handleChangeInput,
+    handleClickButton,
+}) {
+    const [error, setError] = useState(ErrMsg.USER.NO)
 
     const handleUsernameChange = (e) => {
-        const newValue = e.target.value;
-        handleChangeInput(e, "username");
-    };
+        e.preventDefault()
+        const username = e.target.value.trim()
 
-
-    const checkInput = () => {
-        const username = data.username;
+        let haveForbidChar = false
+        for (let i = 0; i < Const.USER.FORBID_CHARS.length; i++) {
+            const char = Const.USER.FORBID_CHARS.slice(i, i + 1)
+            if (username.indexOf(char) >= 0) {
+                setError(ErrMsg.COMMON.FORBID(char))
+                haveForbidChar = true
+            }
+        }
 
         if (!username) {
-            setError("هی! اول یه نام کاربری خوشگل برامون بنویس");
-            return;
-        } else if (username.length < min_length) {
-            setError(` خیلی کوتاهه! حداقل ${min_length - username.length} حرف دیگه لازمه `);
-            return;
-        } else if (username.length > max_length) {
-            setError(`وای! نام کاربری طولانی شد! نهایت ${max_length} حرف جا داره`);
-            return;
-        } else {
-            setError("");
-            handleClickButton();
+            setError(ErrMsg.USER.NO)
+        } else if (username.length < Const.USER.MIN) {
+            setError(ErrMsg.USER.SHORT(username.length))
+        } else if (username.length > Const.USER.MAX) {
+            setError(ErrMsg.USER.LONG(username.length))
+        } else if (!haveForbidChar) {
+            setError("")
         }
+
+        handleChangeInput(e.target.value, "username")
     }
 
     return (
         <div className='flex flex-col gap-7'>
             <div className='flex flex-col gap-1'>
-                <FloatInput label="نام کاربری" value={data.username} required={true}
-                    onChange={(e) => handleUsernameChange(e)}
+                <FloatInput
+                    type={"text"}
+                    label='نام کاربری'
+                    value={userInput.username}
+                    onInput={(e) => handleUsernameChange(e)}
                 />
                 <div className='h-6'>
-                    {
-                        error && (
-                            <p className='text-sm text-right pt-1 px-1 text-red-500 font-medium'>
-                                {error}
-                            </p>
-                        )
-                    }
+                    <p className='text-sm text-right pt-1 px-1 text-red-500 font-medium'>
+                        {error}
+                    </p>
                 </div>
             </div>
             <div className='flex flex-col gap-5'>
-                <button
-                    type="submit"
-                    onClick={checkInput}
-                    className={`bg-purple-500  rounded-lg text-[16px] font-medium w-full
-                         hover:bg-purple-700 text-white py-2 transition-all duration-300`}
-                >
-                    ورود
-                </button>
-                <Link>
-                    <button
-                        type="submit"
-                        className="bg-purple-700 rounded-lg text-[16px]
-                 flex flex-row-reverse justify-center gap-1 
-                 font-medium w-full hover:bg-purple-700 text-white py-2
-                  transition-all duration-300"
-                    >
-                        ورود با
-                        <span>Souperlopers</span>
-                    </button>
-                </Link>
+                <LoginButton
+                    text='ورود'
+                    onClick={handleClickButton}
+                    disable={error.length > 0}
+                />
+                <LoginButton text='ورود با Souperlopers' disable={true} />
             </div>
         </div>
     )
 }
-
-export default GetUsername;
