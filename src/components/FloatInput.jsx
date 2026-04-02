c87391242bf11578c543ce4adb8cc3eaedf88c45
Zahra Rostami <zahrarostami137878@gmail.com>
2026-04-02 20:03:15 +0330

    update: set rtl and ltr direction

--- src/components/FloatInput.jsx
+++ src/components/FloatInput.jsx
@@ -13,12 +13,12 @@ export default function FloatInput({ type, value, onChange, label, onKeyDown }) 
 				onChange={onChange}
 				onKeyDown={onKeyDown}
 				className={`peer w-full h-11 rounded-lg border px-3 bg-(--bg-container) text-neutral-500 pt-1 font-semibold text-[14px] ring-1 ring-purple-300 focus:outline-none focus:ring-2 focus:ring-purple-600 ${hasValue ? "border-purple-600" : "border-[#E2E8F0]"}`}
-				dir="rtl"
+				dir={`${ltr ? "ltr" : "rtl"}`}
 			/>
 			<label
 				style={{ pointerEvents: "none" }}
 				className={`
-                    absolute -top-3 right-3 px-1 font-semibold bg-white rounded-xl
+                    absolute -top-3 ${ltr ? "left-3" : "right-3"} px-1 font-semibold bg-white rounded-xl
                     text-[15px] transition-all duration-300 text-neutral-700
                     bg-(--bg-container)
                     ${hasValue ? "-top-2.5 text-purple-600 bg-white rounded-xl" : "top-2.5"}
@@ -31,7 +31,7 @@ export default function FloatInput({ type, value, onChange, label, onKeyDown }) 
 			</label>
 			{type === "password" && (
 				<button
-					className={`text-3xl absolute left-3 top-2`}
+					className={`text-3xl absolute ${ltr ? "right-3" : "left-3"} top-2`}
 					onClick={() => setDisplayPassword(!displayPassword)}
 				>
 					{displayPassword ? "🙂" : "🫣"}
