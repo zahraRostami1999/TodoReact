c54fdd61adca6902257cd525c0146ec78114d5b4
Zahra Rostami <zahrarostami137878@gmail.com>
2026-04-02 20:04:04 +0330

    add: compnent to handle expand and collapse long texts

---
+++ src/pages/todo/components/ExpandLongText.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+function ExpandLongText({ max_char, text, is_expand, handleClickIsExpand }) {
+    return (
+        <div dir='rtl'>
+            {is_expand ? (
+                <p onClick={() => handleClickIsExpand(is_expand)}>
+                    {text}
+                </p>
+            ) : (
+                <div className='flex gap-1'>
+                    <p>
+                        {text.slice(0, max_char)}
+                    </p>
+                    <span className='cursor-pointer' onClick={() => handleClickIsExpand(is_expand)}>
+                        ...
+                    </span>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default ExpandLongText
