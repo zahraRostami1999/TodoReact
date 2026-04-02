86800a017d46a4bdc0eb989019a16525a82b292f
Zahra Rostami <zahrarostami137878@gmail.com>
2026-04-02 20:03:04 +0330

    set rtl and ltr direction

--- src/components/InputTasks.jsx
+++ src/components/InputTasks.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 const InputTasks = () => {
     const [tasks, setTasks] = useState([]);
     const [newTaskTitle, setNewTaskTitle] = useState("");
+    const ltr = newTaskTitle && !/^[\u0600-\u06FF]/.test(newTaskTitle)
 
     const handleInputChange = (e) => {
         setNewTaskTitle(e.target.value);
@@ -16,7 +17,7 @@ const InputTasks = () => {
     };
 
     return (
-        <div className="w-full flex justify-center flex-wrap p-1">
+        <div className={`w-full flex justify-center flex-wrap p-1`} dir={ltr ? "rtl" : "ltr"}>
             <div className="flex items-center w-full lg:max-w-3xl xl:max-w-3xl md:max-w-3xl sm:max-w-2xl bg-white rounded-full shadow-lg overflow-hidden 
                      focus-within:outline-none focus-within:ring-4 focus-within:ring-orange-600/30 focus-within:border-orange-300
                      focus-within:shadow-[0_0_0_6px_rgba(249,115,22,0.25)]">
@@ -29,20 +30,19 @@ const InputTasks = () => {
                 </button>
                 <input
                     type="text"
-                    className="flex-grow px-6 py-1 text-xl text-right lg:text-3xl font-medium text-gray-700 placeholder-gray-400
+                    className="flex-grow px-6 py-1 text-xl lg:text-3xl font-medium text-gray-700 placeholder-gray-400
                      border border-transparent rounded-full
                      bg-transparent outline-none"
                     placeholder="چیکار باید بکنیم امروز؟"
+                    dir={`${ltr ? "ltr" : "rtl"}`}
                     value={newTaskTitle}
                     onChange={handleInputChange}
                     onKeyDown={(e) => {
                         if (e.key === "Enter") addTask();
                     }}
                 />
-
-
             </div>
-        </div>
+        </div >
     );
 };
 
