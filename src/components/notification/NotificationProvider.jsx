ccef1e22b14a01490813d2468a8b354f7f96cb8c
Zahra Rostami <zahrarostami137878@gmail.com>
2026-05-16 11:39:31 +0330

    feat(NotificationProvider): define provider

---
+++ src/components/notification/NotificationProvider.jsx
@@ -0,0 +1,21 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const NotificationProvider = () => {
+  return (
+    <ToastContainer
+      position="bottom-left"
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop={true}
+      closeOnClick
+      rtl={true}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
+  );
+};
+
+export default NotificationProvider;
