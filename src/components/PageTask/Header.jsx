7d08180dc395e891707c729c92ba2d705ce44054
Zahra Rostami <zahrarostami137878@gmail.com>
2026-05-24 11:50:26 +0330

    style(Header): add blur back of title and logout icon

--- src/components/PageTask/Header.jsx
+++ src/components/PageTask/Header.jsx
@@ -16,17 +16,19 @@ const Header = () => {
 				<div className='font-sans font-semibold flex justify-center flex-w w-full'>
 					<div className='w-full sm:text-center xs:text-center md:text-left lg:text-left '>
 						<div className='flex items-center justify-between'>
-							<h1 className='sm:text-3xl text-neutral-700 md:text-4xl lg:text-5xl font-bold '>
-								HISTX To-Do List
-							</h1>
 							<h1
-								className='text-4xl hover:scale-95 cursor-pointer transition-all duration-300'
+								className='text-4xl hover:scale-95 cursor-pointer transition-all duration-300 
+								backdrop-blur-md bg-white/5'
 								onClick={logout}
 							>
 								<IoIosLogOut
 									size={28}
 									className='hover:text-red-500 transition-all duration-300'
 								/>
+							</h1>
+							<h1 className='relative sm:text-3xl font-[vazir] text-neutral-700 md:text-4xl
+							 lg:text-5xl font-bold py-2 backdrop-blur-md bg-white/10'>
+								لیست وظایف
 							</h1>
 						</div>
 					</div>
